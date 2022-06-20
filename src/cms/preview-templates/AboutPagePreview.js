108e7ac4a709ec0ca22c641e8fd2f1ab0b147a99
Denise Lashlley <htmlash@gmail.com>
2022-06-20 12:45:48 +0100

    fix preview pages

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,15 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import AboutPage from "../../templates/about-page";
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPage
-    page={{
-      frontmatter: entry.getIn(["data"]).toJS(),
-      html: entry.getIn(["data", "body"]),
-      bodyIsMarkdown: true,
-    }}
-  />
-);
+const AboutPagePreview = ({ entry, widgetFor }) => {
+  const data = entry.getIn(["data"]).toJS();
+   return <AboutPage data={data} content={widgetFor('body')} />;
+};
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
