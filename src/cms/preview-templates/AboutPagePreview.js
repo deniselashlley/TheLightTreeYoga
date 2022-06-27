fe71d0f78653f37513ac27b3bca59315e1ec8555
Denise Lashlley <htmlash@gmail.com>
2022-06-27 12:47:05 +0100

    update cms packages

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,14 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import AboutPage from "../../templates/about-page";
 
-const AboutPagePreview = ({ entry }) => (
-   <AboutPage page={{
+const AboutPagePreview = ({ entry,  widgetFor }) => {
+   console.log(entry, widgetFor);
+   return ( <AboutPage page={{
     frontmatter: entry.getIn(["data"]).toJS(),
     html: entry.getIn(["data", "body"]),
     bodyIsMarkdown: false,
   }} 
-  />
-);
+  /> )
+};
 
 
 AboutPagePreview.propTypes = {
