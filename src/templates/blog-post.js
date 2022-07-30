6e597d50da3c6e80870e614233eb243ec762b0ff
Denise Lashlley <htmlash@gmail.com>
2022-07-31 00:49:44 +0100

    add blog styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -14,19 +14,12 @@ export const BlogPostTemplate = ({
   const PostContent = contentComponent || Content
 
   return (
-    <section className="section">
+    <>
       {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <PostContent content={content} />
-          </div>
-        </div>
+      <div className="container blog-page section">
+        <PostContent content={content} />
       </div>
-    </section>
+    </>
   )
 }
 
